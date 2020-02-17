6c0b69cfe96117a260698fd552b5a2fcc373388a
duychu <duycb92@gmail.com>
2020-02-17 17:39:07 +0000

    separate production and development docker file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # django-docker
 Dockerize the django with custom django project
+
+
+docker-compose.exe -f .\docker-compose.prod.yml down -v
+docker-compose.exe -f .\docker-compose.prod.yml build
+docker-compose.exe -f .\docker-compose.prod.yml up -d
+
+docker-compose.exe -f .\docker-compose.prod.yml exec web python manage.py migrate --noinput
--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -72,9 +72,13 @@ WSGI_APPLICATION = 'hello_django.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
